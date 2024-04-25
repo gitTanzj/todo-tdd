bc2ab3199dddf86615ec7a228f9289f1fb0ef66a
gitTanzj <kalle.riit@gmail.com>
2024-04-25 15:27:38 +0300

    Create error handling for unit and integration tests

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.use((error, req, res, next) => {
+    //console.log(error)
+    res.status(500).json({ message: error.message });
+})
+
 
 // app.listen(3015, () => {
 //     console.log('Server is running on http://localhost:3015')
