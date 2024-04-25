eaa6c00f7a9909c78b5b5391a808f8be209ce594
gitTanzj <kalle.riit@gmail.com>
2024-04-25 14:26:40 +0300

    Created TodoController.createTodo integration test and fixed unit test for database

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const todoRoutes = require('./routes/todo.routes')
 const app = express()
+const mongodb = require('./mongodb/mongodb.connect');
+
+mongodb.connect();
+
+app.use(express.json())
+
+app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 
-app.listen(3015, () => {
-    console.log('Server is running on http://localhost:3015')
-})
+// app.listen(3015, () => {
+//     console.log('Server is running on http://localhost:3015')
+// })
+
+module.exports = app
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -3,7 +3,7 @@ const TodoModel = require('../../models/todo.model')
 const httpMocks = require('node-mocks-http')
 const newTodo = require('../mock-data/new-todo.json')
 
-TodoModel.find = jest.fn()
+TodoModel.create = jest.fn()
 
 let req, res, next
 beforeEach(() => {
@@ -20,18 +20,18 @@ describe('TodoController.createTodo', () => {
     it('should have a createTodo function', () => {
         expect(typeof TodoController.createTodo).toBe('function')
     })
-    it('should call TodoModel.create', () => {
-        TodoController.createTodo(req, res, next)
+    it('should call TodoModel.create', async () => {
+        await TodoController.createTodo(req, res, next)
         expect(TodoModel.create).toBeCalledWith(newTodo)
     })
-    it('should reutrn 201 response code', () => {
-        TodoController.createTodo(req, res, next)
+    it('should reutrn 201 response code', async () => {
+        await TodoController.createTodo(req, res, next)
         expect(res.statusCode).toBe(201)
         expect(res._isEndCalled()).toBeTruthy()
     })
-    it('should return json body in response', () => {
-        TodoModel.create.mockReturnValue(newTodo)
-        TodoController.createTodo(req, res, next)
+    it('should return json body in response', async () => {
+        await TodoModel.create.mockReturnValue(newTodo)
+        await TodoController.createTodo(req, res, next)
         expect(res._getJSONData()).toStrictEqual(newTodo)
     })
 })
